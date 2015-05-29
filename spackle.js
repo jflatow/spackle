7ec66ca359a83e424f9f82c8c713606d8ab3bb95
Jared Flatow <jflatow@gmail.com>
2015-05-29 00:13:25 -0700

    new version...

--- spackle.js
+++ spackle.js
@@ -1,10 +1,15 @@
 #!/usr/bin/env node
 
 function usage(unknown) {
-  return 'USAGE: spackle [--help] [--sourcemapfile FILE] FILES ...\n'
+  return 'USAGE: spackle [--help] [--sourcemapfile FILE] FILES ...'
 }
 
-function main(argv) {
+function exit(reason, code) {
+  process.stdout.write(reason + '\n')
+  process.exit(code)
+}
+
+function pack_js(argv) {
   var browserify = require('browserify')
   var bundler = new browserify({debug: true})
   var bundled = argv._.map(bundler.add.bind(bundler))
@@ -35,10 +40,22 @@ if (require.main === module) {
     }
   }
 
+  var path = require('path')
   var argv = require('minimist')(process.argv.slice(2), spec)
-  if (unknown || argv.help) {
-    process.stdout.write(usage(unknown))
+  if (argv.help) {
+    exit(usage(unknown))
+  } else if (unknown) {
+    exit(usage(unknown), 1)
+  } else if (argv._.length < 1) {
+    exit('Not enough arguments', 1)
   } else {
-    main(argv).pipe(process.stdout)
+    var ext = path.extname(argv._[0])
+    switch (ext) {
+    case '.js':
+      pack_js(argv).pipe(process.stdout)
+      break
+    default:
+      exit('Unrecognized file type: ' + argv._[0], 1)
+    }
   }
 }
